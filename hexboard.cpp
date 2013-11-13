eb4a6a0b1da0a1c29f6ad4b6d273ff566164a719
José Luis González <jlg@jlgdev.(none)>
2013-11-13 10:48:49 -0600

    Work in progress: hex

--- hexboard.cpp
+++ hexboard.cpp
@@ -1,16 +1,24 @@
+#include <iostream>
 #include "hexboard.h"
+
 
 HexBoard::HexBoard(int n)
 {
     if (n < 2) throw HEXBOARD_ERR_INVALIDSIZE;
     
-    TOP    = n * n + 0;                 // virtual TOP cell
-    BOTTOM = n * n + 1;                 // virtual BOTTOM cell
-    LEFT   = n * n + 2;                 // virtual LEFT cell
-    RIGHT  = n * n + 3;                 // virtual RIGHT cell
+    int nsquared = n * n;
     
-    board.Reset(n * n + 4);             // board has size (n * n) + 4 virtual nodes
-    cells.Resize(n * n + 4);            // cells array has same size as board
+    size   = n;
+    TOP    = nsquared + 0;              // virtual TOP cell
+    BOTTOM = nsquared + 1;              // virtual BOTTOM cell
+    LEFT   = nsquared + 2;              // virtual LEFT cell
+    RIGHT  = nsquared + 3;              // virtual RIGHT cell
+    
+    IDMAXCELL = nsquared;               // ID of max regular cell, excluding 4 virtual cells
+    
+    board.Reset(nsquared + 4);          // board has size (n * n) + 4 virtual nodes
+    cells.resize(nsquared + 4);         // cells array has same size as board
+    pUF = new UnionFind(nsquared + 4);  // UF has same size as board
     
     for (int r = 0; r < size; r++)
     {
@@ -58,7 +66,13 @@ HexBoard::HexBoard(int n)
     REDHOME  = LEFT;
     REDGOAL  = RIGHT;
     
+    nBlank   = nsquared;        // initially, all cells start blank
     nextMove = HEXBLUE;         // blue starts
+}
+
+HexBoard::~HexBoard(void)
+{
+    if (pUF) delete pUF;
 }
 
 inline int HexBoard::cellIndex(int row, int col) 
@@ -69,6 +83,124 @@ void HexBoard::connect(CellID c1, CellID c2)
 
 HexMoveResult HexBoard::Move(HexBoardColor turn, int row, int col)
 {
+    if (turn != nextMove)
+        return HEXMOVE_INVALIDTURN;
+        
+    if (!checkRowCol(row, col))
+        return HEXMOVE_INVALIDCELL;
+        
+    CellID idCell = cellIndex(row, col);
+    
+    if (cells[idCell].color != HEXBLANK)
+        return HEXMOVE_OCCUPIED;
+        
+    NeighborSet *pN = board.Neighbors(idCell);
+    bool fAdjacentOk = false;
+    for (int i = 0; i < pN->size(); i++)
+    {
+        CellID idNeighbor = (*pN)[i].idNeighbor;
+        if ((idNeighbor >= 0) && (cells[idNeighbor].color == turn))
+        {
+            fAdjacentOk = true;
+            pUF->Join(idNeighbor, idCell);
+        }
+    }
+    delete pN;
+    
+    if (!fAdjacentOk)
+        return HEXMOVE_NOTADJACENT;
+        
+    cells[idCell].color == turn;
+    nBlank--;
+    
+    CellID idGoal = ((turn == HEXBLUE) ? BLUEGOAL : REDGOAL);
+    
+    if (pUF->Find(idGoal) == pUF->Find(idCell))
+        return HEXMOVE_WINNER;
+        
+    if (nBlank == 0)
+        return HEXMOVE_DRAW;
+        
+    nextMove = ((nextMove == HEXBLUE) ? HEXRED : HEXBLUE);
+    
+    return HEXMOVE_OK;
 }
 
+inline HexBoardColor HexBoard::Turn(void)
+{   return nextMove;    }
+
+inline int HexBoard::dim(void)
+{   return size;    }
+
+HexBoardColor HexBoard::Color(int row, int col)
+{
+    if (!checkRowCol(row, col)) return HEXNULL;
+    return cells[cellIndex(row, col)].color;
+}
+
+bool HexBoard::checkRowCol(int row, int col)
+{   return ((row >= 0) && (row < size) && (col >= 0) || (col < size));  }
     
+
+HexBoardIO::HexBoardIO(HexBoard &game) : board(game) {}
+
+HexBoardIO::~HexBoardIO(void)
+{
+}
+
+void HexBoardIO::prompt(void)
+{
+    std::cout << "Please enter your move in row col format.\n";
+    std::cout << "Example: 0 A\n";
+    std::cout << ((board.Turn() == HEXBLUE) ? "BLUE" : "RED") << ", please enter your move: ";
+}
+
+bool HexBoardIO::parse(const std::string r, const std::string c, int &row, int &col)
+{
+    return false;
+}
+
+void HexBoardIO::print(void)
+{
+    int size = board.dim();
+    
+    // print column headers
+    for (int c = 0; c < size; c++)
+        std::cout << "   ";
+        
+    std::cout << "   ";
+    
+    for (int c = 0; c < size; c++)
+    {
+        char ch = 'A' + c;
+        std::cout << " " << ch << " ";
+    }
+    std::cout << "\n";
+    
+    for (int r = 0; r < size; r++)
+    {
+        for (int j = size - r; j > 0; j--)
+            std::cout << "   ";
+            
+        for (int c = 0; c < size; c++)
+        {
+            if (c == 0)
+            {
+                if (r < 10) std::cout << " ";
+                std::cout << r << " ";
+            }
+
+            std::cout << " ";            
+            HexBoardColor clr = board.Color(r, c);
+            if (clr == HEXBLUE) std::cout << "X";
+            else if (clr == HEXRED) std::cout << "O";
+            else std::cout << ".";
+            std::cout << " ";
+
+        }
+        std::cout << "\n";
+    }
+    
+    std::cout << "\nIt is now " << ((board.Turn() == HEXBLUE) ? "BLUE" : "RED") << "'s turn:\n";
+
+}
--- hexboard.h
+++ hexboard.h
@@ -2,19 +2,25 @@
 #define _HEXBOARD_H_
 
 #include <vector>
+#include <string>
+
+#include "unionfind.h"
 #include "graph.h"
 
 
-enum HexBoardColor { HEXBLANK, HEXBLUE, HEXRED };
+enum HexBoardColor { HEXNULL = 0, HEXBLANK, HEXBLUE, HEXRED };
 
+static const int _HexErrBase = 0x122;
 enum HexBoardError {
-    HEXBOARD_ERR_OUTOFMEMORY,
-    HEXBOARD_ERR_INVALIDSIZE
+    HEXBOARD_ERR_OUTOFMEMORY = _HexErrBase,
+    HEXBOARD_ERR_INVALIDSIZE,
+    HEXBOARD_ERR_INVALIDCELL
 };
 
 enum HexMoveResult {
     HEXMOVE_OK,
     HEXMOVE_INVALIDTURN,
+    HEXMOVE_INVALIDCELL,
     HEXMOVE_NOTADJACENT,
     HEXMOVE_OCCUPIED,
     HEXMOVE_WINNER,
@@ -36,9 +42,13 @@ class HexBoard {
 
     public:
     HexBoard(int n);
-    HexBoardColor color(CellID idCell);
+    ~HexBoard(void);
     
-    HexMoveResult Move(HexBoardColor turn, int row, int col);
+    HexBoardColor Color(int row, int col); // return the color of the given cell (HEXBLANK, HEXBLUE, HEXRED, or HEXNULL if error)
+    HexBoardColor Turn(void);           // return whose turn it is to play
+    HexMoveResult Move(HexBoardColor turn, int row, int col);   // place chip of given color at (row, col)
+    
+    int dim(void);                      // dimension of the board (in cells per side)
     
     private:    
     Graph               board;          // Graph underlying structure for path management
@@ -50,11 +60,34 @@ class HexBoard {
     int TOP, BOTTOM, LEFT, RIGHT;       // index of 4 virtual cells
     int BLUEHOME, REDHOME;              // starting location for blue and red
     int BLUEGOAL, REDGOAL;              // target location for blue and red
+    
+    int IDMAXCELL;                      // ID of max regular cell on the board (excluding virtual cells)
+    
+    int nBlank;                         // number of blank cells remaining
+    
+    UnionFind *pUF;                     // compute winners easily
 
     
     int cellIndex(int row, int col);    // given a (row, column) pair, compute index of node
     void connect(CellID c1, CellID c2); // create an edge between cells c1 and c2
+    bool checkRowCol(int row, int col); // check that a given (row, column) pair is within board size
     
+    
+    friend class HexBoardIO;
+    
+};
+
+class HexBoardIO {
+    public:
+    HexBoardIO(HexBoard &game);
+    ~HexBoardIO(void);
+    void print(void);
+    void prompt(void);
+    bool parse(const std::string r, const std::string c, int &row, int &col);
+    void moveFeedback(HexMoveResult result);
+    
+    private:
+    HexBoard &board;
 };
 
 
