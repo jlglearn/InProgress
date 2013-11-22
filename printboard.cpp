c1afe3dd8e4df3c8b42a0f591c043d8c8d5b9aff
José Luis González <jlg@ZHODB.(none)>
2013-11-22 17:02:12 -0600

    Rewrite printboard.cpp

--- printboard.cpp
+++ printboard.cpp
@@ -1,106 +1,52 @@
 #include <iostream>
-#include <cstring>
 
-void printBoard(int size)
+void printboard()
 {
-    std::string s = "";
-    std::string sMargin = "      ";
-    std::string sShift  = "  ";
-
-    // ------------ board header ----------------------------------
-    std::cout << "\n\n" << sMargin
-              << "BLUE moves left to right, RED moves bottom to top\n\n";
-    
-    // ------------ column header ---------------------------------
-    
-    int margin = ((4 * size) - strlen("RED GOAL")) / 2;
-    
-    s = "";
-    for (int i = 0; i < margin; i++)
-        s = s + " ";
-        
-    std::cout << "\n\n" << sMargin;    
-    std::cout << s << "RED GOAL\n";
+    int size = 11;
+    char sMargin[] = "    ";
     
     std::cout << sMargin;
+    for (int i = 0; i < (3 + 1); i++)
+        std::cout << " ";    
     for (int i = 0; i < size; i++)
-    {
-        std::cout << " "
-                  << ((i < 10) ? " " : "")
-                  << i
-                  << " ";
-    }
+        std::cout << " " << i << " ";
     std::cout << "\n";
     
-    // ------------ top border ------------------------------------
-    std::cout << sMargin << "  ";
+    std::cout << sMargin;
+    for (int i = 0; i < (3 + 1); i++)
+        std::cout << " ";        
+    for (int i = 0; i < (2 + 3 * size + 1); i++)
+        std::cout << "-";        
+    std::cout << "\n";
     
-    for (int i = 0; i < size; i++)
-        std::cout << "----";
-        
-    std::cout << "\n";    
-    
-    // ------------ board rows ------------------------------------
-    
-    int k = size / 2;
-    
-    s = "";
-    for (int i = 0; i < size; i++)
+    for (int row = 0; row < size; row++)
     {
-        std::cout << s
-                  << ((i == k) ? "BLUE" : 
-                      ((i == (k + 1)) ? "HOME" : "    " ))
-                  << "  "
-                  << ((i < 10) ? " " : "")
-                  << i
-                  << " ";
-                  
-        for (int j = 0; j < size; j++)
-        {
-            std::cout << "  "
-                      << "."        // chip
-                      << " ";
-        }
-        
-        std::cout << " " 
-                  << i << "   "
-                  << ((i == k) ? "BLUE" :
-                      ((i == (k + 1)) ? "GOAL" : ""))
-                  << "\n";
-        s = s + sShift;
+        std::cout << sMargin;
+        for (int s = 0; s < row; s++)
+            std::cout << " ";
+        std::cout << ((row < 10) ? "  " : " ") << row << " " << "\\ ";
+        for (int col = 0; col < size; col++)
+            std::cout << " . ";
+        std::cout << " \\ " << row << "\n";
     }
     
-    // ------------ bottom border -----------------------------------
-    std::cout << sMargin << s << sShift;
-    
+    std::cout << sMargin;
+    for (int i = 0; i < (size + 3 + 1); i++)
+        std::cout << " ";
+    for (int i = 0; i < (2 + 3 * size + 1); i++)
+        std::cout << "-";
+    std::cout << "\n";   
+
+    std::cout << sMargin;
+    for (int i = 0; i < (size + 3 + 1 + 2); i++)
+        std::cout << " ";
     for (int i = 0; i < size; i++)
-        std::cout << "----";
-        
-    std::cout << "\n";  
-    
-    // ------------ column footer -----------------------------------
-    std::cout << sMargin << s << "   ";              
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << " "
-                  << ((i < 10) ? " " : "")
-                  << i
-                  << " ";
-    }
-    std::cout << "\n";    
-    
-    std::cout << sMargin << s << "   ";
-    
-    s = "";
-    for (int i = 0; i < margin; i++)
-        s = s + " ";
-        
-    std::cout << s << "RED HOME\n\n\n";    
-    
+        std::cout << " " << i << " ";
+    std::cout << "\n";
 }
 
 int main(int argc, char *argv[])
 {
-    printBoard(11);
+    printboard();
     return 0;
 }
